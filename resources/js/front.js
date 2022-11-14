25c1b90657153f68ce3eaed07a9df2206ce7bf6a
AlexBosio95 <alex.bosio1995@gmail.com>
2022-11-14 16:02:38 +0100

    feat: vue axios all garages paginate

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,3 +1,6 @@
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 import Vue from 'vue';
 import router from './router';
 import App from './views/App';
