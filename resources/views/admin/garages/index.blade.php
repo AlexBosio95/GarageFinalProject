61388fdae4ec384e7038485510bb9fa9d246f447
Valerio <canivalerio93@gmail.com>
2022-11-14 11:04:28 +0100

    feat: correction index page: removed index of garage

--- resources/views/admin/garages/index.blade.php
+++ resources/views/admin/garages/index.blade.php
@@ -8,19 +8,19 @@
         {{ session('img-removed') }}
     </div>
   @endif
-  
+
   @if (session('created'))
     <div class="alert alert-success">
         {{ session('created') }}
     </div>
   @endif
-  
+
   @if (session('edited'))
     <div class="alert alert-success">
         {{ session('edited') }}
     </div>
   @endif
-  
+
   @if (session('deleted'))
     <div class="alert alert-danger">
         {{ session('deleted') }}
@@ -33,33 +33,31 @@
             <table class="table table-striped">
                 <thead>
                   <tr>
-                    <th scope="col">ID</th>
                     <th scope="col">Title</th>
                     <th scope="col" class="small-none text-center">Square Meters</th>
                     <th scope="col" class="small-none text-center">Address</th>
                     <th scope="col" class="text-center">Actions</th>
-                    <th scope="col">Available</th>               
+                    <th scope="col">Available</th>
                   </tr>
                 </thead>
                 <tbody>
                     @foreach ($garages as $garage)
                         <tr>
-                          <th scope="row">{{$garage->id}}</th>
                           <td>{{$garage->title}}</td>
                           <td class="small-none text-center">{{$garage->sqmt}}</td>
                           <td class="small-none">{{$garage->address}}</td>
                           <td class="d-flex justify-content-between flex-wrap">
-                            <a href="{{ route('admin.garages.show',['garage'=>$garage]) }}" class="btn btn-primary m-1"> show </a>                      
+                            <a href="{{ route('admin.garages.show',['garage'=>$garage]) }}" class="btn btn-primary m-1"> show </a>
                             <a href="{{ route('admin.garages.edit',['garage'=>$garage]) }}" class="btn btn-primary m-1"> Edit </a>
-                          
+
                             <form action="{{ route('admin.garages.destroy',['garage'=>$garage]) }}" method="POST">
                               @csrf
                               @method('DELETE')
 
-                              <button type="submit" class="btn btn-danger m-1" onclick="return confirm('Are you sure? This action is irreversible')"> Delete </button>                    
-          
+                              <button type="submit" class="btn btn-danger m-1" onclick="return confirm('Are you sure? This action is irreversible')"> Delete </button>
+
                             </form>
-                          </td>  
+                          </td>
                           <td>
                             @if ($garage->available)
                                 <span class="text-success">
@@ -70,7 +68,7 @@
                                 Not available
                               </span>
                             @endif
-                          </td>   
+                          </td>
                         </tr>
                     @endforeach
                 </tbody>
