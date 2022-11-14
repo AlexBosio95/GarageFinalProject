ea498abd20fb939ba0a4a87a29e13bb1e761ddbd
AlexBosio95 <alex.bosio1995@gmail.com>
2022-11-14 12:48:25 +0100

    feat: vue router

---
+++ resources/js/pages/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+
+        <h1>HomePage</h1>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'MainPost',
+}
+</script>
+
+<style>
+</style>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -5,19 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Benvenuti in frontend</title>
+    <title>Garages</title>
     <link href="{{asset('css/app.css')}}" rel="stylesheet" />
 </head>
 <body>
-    <div class="text-right m-4">
-        
+    {{-- <div class="text-right m-4">
         <a class="btn btn-primary " href="{{ route('login') }}">{{ __('Login') }}</a>   
-    
-    </div>
-    
-    <h1 class="text-center">Ciao PM!</h1>
-    {{-- <div id="root"></div>
-    <script src="{{asset('js/front.js')}}"></script> --}}
+    </div> --}}
+    <div id="root"></div>
+    <script src="{{asset('js/front.js')}}"></script>
 </body>
 </html>
 
