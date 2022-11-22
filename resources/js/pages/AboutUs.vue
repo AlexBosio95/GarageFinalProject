c95aeba0b0d4dc8eb1f80199728ee4ad8b375109
Marco Valerio <mo.valerio93@gmal.com>
2022-11-22 17:43:21 +0100

    feat: about us text fixed

--- resources/js/pages/AboutUs.vue
+++ resources/js/pages/AboutUs.vue
@@ -3,10 +3,10 @@
 
         <h1 class="text-center m-5">About<span class="my-us">Us</span></h1>
         <p>        
-            Garage finder è nato come progetto sviluppato da un team composto da quattro jr. full stack developer che a conclusione di un percorso formativo durato sei mesi per accquisire competenze di web developer, hanno deciso di sviluppare una web app per affito di garage a breve termine.
+            Garage finder è nato come progetto sviluppato in un team composto da quattro jr. full stack developer, che a conclusione di un percorso formativo per accquisire le competenze da web developer, hanno deciso di sviluppare una web app per affito di garage a breve termine.
         </p>
         <p>
-            Con questo progetto abbiamo messo alla prova le competenze sia front-end che back-end apprese durante il corso, costruendo la parte back-end con laravel e la parte front con vue-router. Grazie all’utilizzo delle api di tomtom-dev siamo riusciti a localizzare i garage in una mappa. Utilizzando html css e bootstrap abbiamo ideato e creato una grafica per il nostro sito.
+            Con questo progetto abbiamo messo alla prova le competenze sia front-end che back-end apprese durante il corso, costruendo la parte back-end con laravel e la parte front con vue-router. Grazie all’utilizzo delle api di tomtom-dev siamo riusciti a localizzare i garage su una mappa. Utilizzando html css e bootstrap abbiamo ideato e creato e costruito un interfacciagrafica per il nostro sito.
         </p>
 
         <h1 class="text-center">Team</h1>
